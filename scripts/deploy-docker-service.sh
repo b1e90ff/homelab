b5ae4038ef0306ab9882cb8468b77ce447a20e0b
tat <niklas@tat.systems>
2024-11-23 23:15:48 +0100

    debög

--- scripts/deploy-docker-service.sh
+++ scripts/deploy-docker-service.sh
@@ -12,4 +12,6 @@ if [ -z "$SERVICE" ] || [ -z "$SSH_USER" ] || [ -z "$SSH_HOST" ] || [ -z "$DEPLO
 fi
 
 echo "Deploying Docker service: $SERVICE"
+echo "sh $SSH_USER@$SSH_HOST 'cd $DEPLOY_PATH/${SERVICE}/. && docker compose pull && docker compose up -d'"
 sh $SSH_USER@$SSH_HOST "cd $DEPLOY_PATH/${SERVICE}/. && docker compose pull && docker compose up -d"
+echo "WTF"
