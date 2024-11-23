bf250f1662c594085f6d23980b438058ecdcf1ce
tat <niklas@tat.systems>
2024-11-23 21:27:11 +0100

    ✨ feat: Remove deploy-service.sh script and update upload-files.sh for directory creation and use rsync; enhance Traefik configuration with logging

--- scripts/upload-files.sh
+++ scripts/upload-files.sh
@@ -10,8 +10,12 @@ if [ -z "$SERVICE" ] || [ -z "$SSH_USER" ] || [ -z "$SSH_HOST" ] || [ -z "$DEPLO
     exit 1
 fi
 
+# Create data directory structure
+echo "Creating data directory for $SERVICE"
+ssh $SSH_USER@$SSH_HOST "mkdir -p $DEPLOY_PATH/../data/$SERVICE"
+
 echo "Processing secrets in yaml files for $SERVICE"
 ./scripts/replace-secrets.sh "$SERVICE" "$DEPLOY_PATH"
 
 echo "Uploading $SERVICE"
-scp -r "${SERVICE}" $SSH_USER@$SSH_HOST:$DEPLOY_PATH/
+rsync -av --delete "${SERVICE}/" $SSH_USER@$SSH_HOST:$DEPLOY_PATH/${SERVICE}/
